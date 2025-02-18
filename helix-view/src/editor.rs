07eed3cc567a31d3645fc6e159c0f6a0eab2122c
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-18 11:28:30 +0000

    style: formatting

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -1071,7 +1071,6 @@ pub struct Editor {
     pub tree: Tree,
     pub next_document_id: DocumentId,
     pub documents: BTreeMap<DocumentId, Document>,
-    pub path_editing: Option<PathBuf>,
 
     // We Flatten<> to resolve the inner DocumentSavedEventFuture. For that we need a stream of streams, hence the Once<>.
     // https://stackoverflow.com/a/66875668
@@ -1137,6 +1136,7 @@ pub struct Editor {
 
     pub mouse_down_range: Option<Range>,
     pub cursor_cache: CursorCache,
+    pub file_explorer_selected_path: Option<PathBuf>,
 }
 
 pub type Motion = Box<dyn Fn(&mut Editor)>;
@@ -1258,7 +1258,7 @@ impl Editor {
             handlers,
             mouse_down_range: None,
             cursor_cache: CursorCache::default(),
-            path_editing: None,
+            file_explorer_selected_path: None,
         }
     }
 
