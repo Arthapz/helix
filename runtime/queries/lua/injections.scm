9344980000a68418d2dcb39ee7012b9062324f52
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-10-19 17:56:35 +0200

    apply patches

--- runtime/queries/lua/injections.scm
+++ runtime/queries/lua/injections.scm
@@ -2,6 +2,30 @@
  (#set! injection.language "comment")
  (#set! injection.include-children))
 
+ 
+ ; string.format("format string", ...)
+ ((function_call
+   name: (dot_index_expression
+     table: (identifier) @_table
+     field: (identifier) @_function)
+   arguments: (arguments
+     .
+     (string
+       content: (string_content) @injection.content)))
+   (#eq? @_table "string")
+   (#eq? @_function "format")
+   (#set! injection.language "lua-format-string"))
+ 
+ ; ("format"):format(...)
+ ((function_call
+   name: (method_index_expression
+     table: (parenthesized_expression
+       (string
+         content: (string_content) @injection.content))
+     method: (identifier) @_function))
+   (#eq? @_function "format")
+   (#set! injection.language "lua-format-string"))
+
 ; string.match("123", "%d+")
 (function_call
   (dot_index_expression
